f9568cd8037593112262dc16b88d54badd12918b
rradeba <rradeba@gmail.com>
2024-11-12 20:44:04 -0500

    c2

--- my-react-app/src/services/orderService.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from './axiosInstance';
-
-export const getOrders = () => {
-  return axiosInstance.get('/orders');  
-};
-
-export const createOrder = (orderData) => {
-  return axiosInstance.post('/orders', orderData);  
-};
